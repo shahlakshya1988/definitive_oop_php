f0142db2e90c13a14fd3e5913cad48ad4eaa0ccf
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-03-07 23:44:32 +0530

    2. Object properties

---
+++ 02/classes/Car.php
@@ -0,0 +1,10 @@
+<?php 
+class Car{
+    /** properties, [variables] , public protected private -> scope modifier*/
+    public $color = "black";
+    public $wheels = 4;
+    public $speed = 180;
+    public $brand;
+    //var $brand; // older version of php
+
+}
